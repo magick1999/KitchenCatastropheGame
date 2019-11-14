b864172b43313e9b700445d4724026706509b7f7
RowanAldean <rowanaldean@gmail.com>
2019-11-14 21:38:58 +0000

    Created the Moveable Object class and provided simple getter and setters with an abstract move method (all objs have diff move patterns hence I did not give any code for move). A default constructor will be used as there is no params for an object constructor anyways - perhaps size? etc.? review please :)
    Casting BorderPane on Main.java where BorderPane is the given type is redundant.

--- source/group44/Main.java
+++ source/group44/Main.java
@@ -1,17 +1,17 @@
 package group44;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainGameWindow.fxml"));
+			BorderPane root = FXMLLoader.load(getClass().getResource("MainGameWindow.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
---
+++ source/group44/MoveableObject.java
@@ -0,0 +1,24 @@
+package group44;
+
+abstract class MoveableObject {
+    private int velocityX;
+    private int velocityY;
+
+    public int getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVelocityX(int velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public int getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(int velocityY) {
+        this.velocityY = velocityY;
+    }
+
+    public abstract void move();
+}
