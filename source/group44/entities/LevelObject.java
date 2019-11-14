24a3aad116b5d1a1e5fa9a58fceb5c3a0e411112
RowanAldean <rowanaldean@gmail.com>
2019-11-14 23:23:35 +0000

    Added Color to this. Still needs finalising

--- source/group44/entities/LevelObject.java
+++ source/group44/entities/LevelObject.java
@@ -1,7 +1,17 @@
 package group44.entities;
 
+import javafx.scene.paint.Color;
+
 class LevelObject {
+    private Color color;
 
     //PLEASE COMPLETE
 
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color newColor) {
+        this.color = newColor;
+    }
 }
