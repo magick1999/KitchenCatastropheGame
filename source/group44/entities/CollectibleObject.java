9bb5ce4d4e1698e5ae789221a25fb3bdb18d631c
RowanAldean <rowanaldean@gmail.com>
2019-11-14 23:19:27 +0000

    Many CollectibleObjects will be on screen at once so need some means of knowing whether to show or not (introduced a boolean to demonstrate this).

---
+++ source/group44/entities/CollectibleObject.java
@@ -0,0 +1,28 @@
+package group44.entities;
+
+public abstract class CollectibleObject {
+    private int xPos;
+    private int yPos;
+    private boolean collected; //if obj iscollected == true -> add to inventory.
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    //many collectible objects will be made and handled at any given time thus must track which are still to be shown.
+    public boolean isCollected() {
+        return collected;
+    }
+}
