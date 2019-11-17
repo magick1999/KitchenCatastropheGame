21fe7f14e724767e107ac4895cedda8032247cc5
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-17 10:18:57 +0000

    Update Enemy.java

--- source/group44/entities/Enemy.java
+++ source/group44/entities/Enemy.java
@@ -1,9 +1,18 @@
 package group44.entities;
 
-import group44.entities.MoveableObject;
+import group44.entities.MovableObject;
 
-public abstract class Enemy extends MoveableObject {
-
+/**
+ * Abstract class for enemies.
+ * 
+ * @author Tomas Svejnoha, Rowan Aldean
+ * @version 1.0
+ */
+public abstract class Enemy extends MovableObject {
+    /**
+     * Attack player if player collided with enemy.
+     * 
+     * @param p - Player
+     */
     public abstract void attackPlayer(Player p);
-
 }
