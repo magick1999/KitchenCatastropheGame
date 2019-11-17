3cda0a65a173a3535de7dc187fed2ec9afaa80a0
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-17 11:25:27 +0000

    Update Main.java

--- source/group44/Main.java
+++ source/group44/Main.java
@@ -1,5 +1,5 @@
 package group44;
-
+	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,16 +11,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("MainGameWindow.fxml"));
-			Scene scene = new Scene(root, 400, 400);
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainGameWindow.fxml"));
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch (Exception e) {
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
