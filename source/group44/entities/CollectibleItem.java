539a07de23a1cf8eee877af9bdb9e16fcc7da31c
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-17 11:45:44 +0000

    Added comment to CollectibleItem

--- source/group44/entities/CollectibleItem.java
+++ source/group44/entities/CollectibleItem.java
@@ -1,6 +1,11 @@
 package group44.entities;
 
+/**
+ * Abstract class for all collectible items in the game.
+ * 
+ * @author Tomas Svejnoha
+ * @version 1.0
+ */
 public abstract class CollectibleItem extends LevelObject {
-    // If the item is collected, it will be removed from the Grid (Cell) and added
-    // to the player's repository
+
 }
