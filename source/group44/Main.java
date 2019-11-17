0e24940b221fddcec01b5b53dbc6fd272a8fbe57
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-17 11:24:11 +0000

    Reverted to original Main.java

--- source/group44/Main.java
+++ source/group44/Main.java
@@ -1,37 +1,27 @@
 package group44;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
-	private static final double width = 400;
-	private static final double height = 400;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = FXMLLoader.load(getClass().getResource("MainGameWindow.fxml"));
-			Scene scene = new Scene(root,width,height);
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("MainGameWindow.fxml"));
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
-	}
-
-	public static double getWidth() {
-		return width;
-	}
-
-	public static double getHeight() {
-		return height;
 	}
 }
