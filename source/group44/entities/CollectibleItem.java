908a54edb8930922bfdc5fe26dd14620353d1107
RowanAldean <rowanaldean@gmail.com>
2019-11-18 20:17:17 +0000

    Sets default for a collectible to a star.

--- source/group44/entities/CollectibleItem.java
+++ source/group44/entities/CollectibleItem.java
@@ -1,11 +1,19 @@
 package group44.entities;
 
+import javafx.scene.image.Image;
+
 /**
  * Abstract class for all collectible items in the game.
  * 
- * @author Tomas Svejnoha
+ * @author Tomas Svejnoha, Rowan Aldean
  * @version 1.0
  */
 public abstract class CollectibleItem extends LevelObject {
+    private String name;
 
+    public CollectibleItem(){
+        String basicPath = "https://p1.hiclipart.com/preview/180/349/86/super-mario-icons-super-mario-star-illustration.jpg";
+        Image basic = new Image(basicPath);
+        this.setImage(basic); //Sets the image of the collectible to a default star.
+    }
 }
