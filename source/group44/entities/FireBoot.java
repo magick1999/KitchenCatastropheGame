569e54ce1b1ee19c9ca34038305bacc2c603f587
RowanAldean <rowanaldean@gmail.com>
2019-11-18 20:16:01 +0000

    Defaults the FireBoot to draw a simple boot png from the internet.

---
+++ source/group44/entities/FireBoot.java
@@ -0,0 +1,19 @@
+package group44.entities;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class FireBoot extends CollectibleItem {
+
+    public FireBoot(){
+        String bootPath = "https://www.clipartroo.com/images/93/firefighter-boots-clipart-93602.png";
+        Image boot = new Image(bootPath);
+        this.setImage(boot);
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+        gc.drawImage(this.getImage(), this.getPositionX(), this.getPositionY());
+    }
+
+}
