e190e3c826939fffb8ce04f57b1a59b3a4470b4e
mason0109 <56236607+mason0109@users.noreply.github.com>
2019-11-19 19:35:17 +0000

    Water and Fire cells.

---
+++ source/group44/entities/Fire.java
@@ -0,0 +1,12 @@
+/**
+ * This class is for the fire cells.
+ * @author Amy Mason
+ * @version 1.0
+ * 
+ */
+public class Fire {
+	private boolean stepable = false;
+	
+	public void Fire() {  }
+	
+}
---
+++ source/group44/entities/Water.java
@@ -0,0 +1,12 @@
+/**
+ * This class is for the water cells.
+ * @author Amy Mason
+ * @version 1.0
+ *
+ */
+public class Water {
+	private boolean stepable = false;
+	
+	public Water () {  }
+	
+}
