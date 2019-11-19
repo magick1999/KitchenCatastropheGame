9929e92a2ba1d3ad7affc135f13db394dc81b8e5
mason0109 <56236607+mason0109@users.noreply.github.com>
2019-11-19 19:08:44 +0000

    Key and Token doors.

---
+++ source/group44/KeyDoor.java
@@ -0,0 +1,24 @@
+
+/**
+ * This class is for the key doors where the player needs a certain coloured 
+ * key is needed to be able to open the door. 
+ * This class also enherits from the superclass Door.
+ * @author Amy Mason
+ * @version 1.0
+ * 
+ */
+public class KeyDoor extends Door {
+	private String colourKeyNeeded;
+	//might want colour if they're not images.
+	//private Color doorColour;
+	
+	/**
+	 * This is the Door constructor, which can be called whenever the object needs to be
+	 * created - once the details have been read in from the textfile.
+	 * @param locked
+	 */
+	public KeyDoor (boolean isOpen, String colourKeyNeeded) {
+		this.colourKeyNeeded = colourKeyNeeded;
+		this.isOpen = isOpen;
+	}
+}
---
+++ source/group44/TokenDoor.java
@@ -0,0 +1,24 @@
+
+/**
+ * This class is for the token doors where the player needs to 
+ * have a certain amount of tokens to be able to open it.
+ * 
+ * @author Amy Mason
+ * @version 1.0
+ * 
+ */
+public class TokenDoor extends Door {
+	private int tokensNeeded; 
+	
+	/**
+	 * This is the Door constructor, which can be called whenever the object needs to be
+	 * created - once the details have been read in from the textfile.
+	 * @param locked
+	 */
+	public TokenDoor (boolean isOpen, int tokensNeeded) {
+		this.tokensNeeded = tokensNeeded;
+		this.isOpen = isOpen;
+	}
+
+	
+}
