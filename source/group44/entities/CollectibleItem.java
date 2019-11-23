44b15a982db6dbb6e851e3427f149f4265fa3e86
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-23 22:27:17 +0000

    Updated constructors in CollectibleItem and MovableObject

--- source/group44/entities/CollectibleItem.java
+++ source/group44/entities/CollectibleItem.java
@@ -5,13 +5,27 @@ import javafx.scene.canvas.GraphicsContext;
 /**
  * Abstract class for all collectible items in the game.
  * 
- * @author Rowan Aldean
+ * @author Tomas Svejnoha, Rowan Aldean
  * @version 1.0
  */
 public abstract class CollectibleItem extends LevelObject {
 
     /**
+     * Creates a new {@link CollectibleItem}.
+     * 
+     * @param title     - Title of the object
+     * @param positionX - Position X in the game
+     * @param positionY - Position Y in the game
+     * @param size      - Size of the cell on the screen
+     * @param imagePath - Image path of the instance
+     */
+    public CollectibleItem(String title, int positionX, int positionY, int size, String imagePath) {
+        super(title, positionX, positionY, size, imagePath);
+    }
+
+    /**
      * Draws collectible using drawImage.
+     * 
      * @param gc The graphics context that is being drawn to.
      */
     @Override
