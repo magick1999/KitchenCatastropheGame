9311fdc8a1068d6b3091b78e7845610c091ee12f
RowanAldean <rowanaldean@gmail.com>
2019-11-23 20:12:28 +0000

    using a local enum to set color of a key or default to GOLD.

--- source/group44/entities/Key.java
+++ source/group44/entities/Key.java
@@ -3,58 +3,45 @@ package group44.entities;
 import javafx.scene.paint.Color;
 
 /**
- * Key class which sets a corresponding image dependent on the color ID of a Key (derived upon instantiation of Key object).
+ * Key class which holds color (JavaFx Color) of the key..
  *
  * @author Rowan Aldean
  * @version 1.0
  */
 public class Key extends CollectibleItem {
-    private int keyColorID; //The color id for the key (1-4)
-    private Color keyColor; //The color corresponding to the id (R,G,B,Gold)
+    private Color keyColor;
+    //The possible colors a key can be.
+    public enum keyColors {
+        RED(Color.RED),
+        BLUE(Color.BLUE),
+        GREEN(Color.GREEN),
+        GOLD(Color.GOLD);
+
+        private Color color;
+
+        keyColors(Color someColor) {
+            this.color = someColor;
+        }
+
+        public Color getColor() {
+            return color;
+        }
+    }
 
     /**
      * A key constructor with no parameters defaults to Yellow/Gold.
      *
      */
     public Key(){
-        deriveKey(4);//4 (anything not 1,2,3) is default gold key.
+        this.keyColor = keyColors.GOLD.getColor();//default gold key.
     }
 
     /**
-     * A Key constructor which takes some keyColorID as a parameter and constructs a corresponding color key.
-     * @param keyColorID the corresponding ID for a key of some color e.g 1 is RED 2 is GREEN 3 is BLUE
+     * A Key constructor which takes some keyColor enum as a parameter and constructs a corresponding color key.
+     * @param givenColor the keyColors enum type color to create.
      */
-    public Key(int keyColorID) {
-        deriveKey(keyColorID);
-    }
-
-    /**
-     * Helper function which takes some int ID and maps the current Key object to the corresponding color.
-     * @param keyColorID the intended ID to set the Key color to.
-     */
-    private void deriveKey(int keyColorID){
-        String keyPath;
-        switch (keyColorID) {
-            case 1:
-                this.keyColor = Color.RED;
-            case 2:
-                this.keyColor = Color.GREEN;
-            case 3:
-                this.keyColor = Color.BLUE;
-            default:
-                System.out.println("Default gold key object created");
-                this.keyColor = Color.GOLD;
-
-        }
-        this.setTitle(keyColor.toString() + " key");
-    }
-
-    /**
-     * Gets the ID of a Key object corresponding to each color.
-     * @return the ID of the Key object.
-     */
-    public int getKeyColorID() {
-        return keyColorID;
+    public Key(keyColors givenColor) {
+        this.keyColor = givenColor.getColor();
     }
 
     /**
