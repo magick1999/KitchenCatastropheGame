bafe0c1bec75b381c2058ffe8d6565318b9dc5bb
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-24 16:33:10 +0000

    Update Cell.java

--- source/group44/entities/Cell.java
+++ source/group44/entities/Cell.java
@@ -3,7 +3,7 @@ package group44.entities;
 /**
  * Abstract class from which all cells inherit.
  * 
- * @author Tomas Svejnoha
+ * @author Tomas Svejnoha, Rowan Aldean
  * @version 1.0
  */
 public abstract class Cell extends LevelObject {
