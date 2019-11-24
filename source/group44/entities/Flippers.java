925fd576c05263589ab8211e9d62267510888af9
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-24 16:06:54 +0000

    Updated Collectible Items

--- source/group44/entities/Flippers.java
+++ source/group44/entities/Flippers.java
@@ -1,17 +1,21 @@
 package group44.entities;
 
 /**
- * Flipper class which sets its name in constructor.
+ * Represents {@link Flippers} in the game.
  *
- * @author Rowan Aldean
+ * @author Tomas Svejnoha, Rowan Aldean
  * @version 1.0
  */
 public class Flippers extends CollectibleItem {
-
     /**
-     * Constructs Flippers object and sets title.
+     * Creates a new instance of {@link Flippers} with position, size, and image.
+     * 
+     * @param positionX - Position X in the game
+     * @param positionY - Position Y in the game
+     * @param size      - Size of the cell on the screen
+     * @param imagePath - Image path of the instance
      */
-    public Flippers(){
-        this.setTitle("flippers");
+    public Flippers(int positionX, int positionY, int size, String imagePath) {
+        super("Flippers", positionX, positionY, size, imagePath);
     }
 }
--- source/group44/entities/Key.java
+++ source/group44/entities/Key.java
@@ -3,53 +3,79 @@ package group44.entities;
 import javafx.scene.paint.Color;
 
 /**
- * Key class which holds color (JavaFx Color) of the key..
+ * Represents a colored {@link Key} in the game.
  *
- * @author Rowan Aldean
+ * @author Tomas Svejnoha, Rowan Aldean
  * @version 1.0
  */
 public class Key extends CollectibleItem {
-    private Color keyColor;
-    //The possible colors a key can be.
-    public enum keyColors {
-        RED(Color.RED),
-        BLUE(Color.BLUE),
-        GREEN(Color.GREEN),
-        GOLD(Color.GOLD);
+    private KeyType keyType;
 
-        private Color color;
+    /**
+     * Creates a new instance of {@link Key} of specific color type.
+     * 
+     * @param type      - Color type of the {@link Key}
+     * @param positionX - Position X in the game
+     * @param positionY - Position Y in the game
+     * @param size      - Size of the cell on the screen
+     */
+    public Key(KeyType type, int positionX, int positionY, int size) {
+        super(type.getTitle(), positionX, positionY, size, type.getImagePath());
+        this.keyType = type;
+    }
 
-        keyColors(Color someColor) {
-            this.color = someColor;
+    /**
+     * Returns the code of the key
+     * 
+     * @return key code represented as {@link Integer}
+     */
+    public int getKeyCode() {
+        return this.keyType.getKeyCode();
+    }
+
+    /**
+     * Represents all possible types of a {@link Key}. {@link KeyType} contains
+     * KeyCode and image path.
+     */
+    public enum KeyType {
+        // TODO: Add image paths
+        RED(1, "Red key", ""), BLUE(2, "Blue key", ""), GREEN(3, "Green key", ""), GOLD(4, "Gold key", "");
+
+        private int code; // used to unlock the door (binary operation)
+        private String title;
+        private String imagePath;
+
+        KeyType(int code, String title, String imagePath) {
+            this.code = code;
+            this.title = title;
+            this.imagePath = imagePath;
         }
 
-        public Color getColor() {
-            return color;
+        /**
+         * Returns the code of the key
+         * 
+         * @return key code represented as {@link Integer}
+         */
+        public int getKeyCode() {
+            return this.code;
+        }
+
+        /**
+         * Returns the title (name) of the key
+         * 
+         * @return the title
+         */
+        public String getTitle() {
+            return this.title;
+        }
+
+        /**
+         * Returns a path to the image of the key
+         * 
+         * @return a path represented as {@link String}
+         */
+        public String getImagePath() {
+            return this.imagePath;
         }
     }
-
-    /**
-     * A key constructor with no parameters defaults to Yellow/Gold.
-     *
-     */
-    public Key(){
-        this.keyColor = keyColors.GOLD.getColor();//default gold key.
-    }
-
-    /**
-     * A Key constructor which takes some keyColor enum as a parameter and constructs a corresponding color key.
-     * @param givenColor the keyColors enum type color to create.
-     */
-    public Key(keyColors givenColor) {
-        this.keyColor = givenColor.getColor();
-    }
-
-    /**
-     * Gets the JavaFX Color of the Key object
-     * @return the color attribute of the Key object.
-     */
-    public Color getKeyColor() {
-        return keyColor;
-    }
-
 }
