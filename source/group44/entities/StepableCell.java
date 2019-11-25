35040ac3c9df86c0855dc963119054826b018749
Jordan <56432409+xrhythmic@users.noreply.github.com>
2019-11-25 11:59:25 +0000

    Update StepableCell.java

--- source/group44/entities/StepableCell.java
+++ source/group44/entities/StepableCell.java
@@ -7,46 +7,56 @@ package group44.entities;
  * @version 1.1
  */
 public abstract class StepableCell extends Cell {
-    
+	
+	/**
+	* Creates a new {@link StepableCell}.
+	* 
+	* @param title     - Title of the object
+	* @param positionX - Position X in the game
+	* @param positionY - Position Y in the game
+	* @param size      - Size of the cell on the screen
+	* @param imagePath - Image path of the instance
+	*/
+
 	public StepableCell(String title, int positionX, int positionY, int size, String imagePath) {
 		super(title, positionX, positionY, size, imagePath);
 	}
 
 	private MovableObject movableObject; // MovableObject standing on the cell
 
-    /**
-     * Places object on the {@link StepableCell}
-     * 
-     * @param object - {@link MovableObject} that steps on the cell.
-     */
-    public void stepOn(MovableObject object) {
-        if (this.getMovableObject() == null) {
-            this.setMovableObject(object);
-        } else {
-            // TODO: Implement
-            throw new UnsupportedOperationException("Not implemented exception");//This would also be the exception if an enemy caught a player.
-        }
-    }
+	/**
+	* Places object on the {@link StepableCell}
+	* 
+	* @param object - {@link MovableObject} that steps on the cell.
+	*/
+	public void stepOn(MovableObject object) {
+	if (this.getMovableObject() == null) {
+	    this.setMovableObject(object);
+	} else {
+	    // TODO: Implement
+	    throw new UnsupportedOperationException("Not implemented exception");//This would also be the exception if an enemy caught a player.
+		}
+	}
 
-    /**
-     * Removes {@link MovableObject} from the {@link StepableCell}.
-     */
-    public void stepOff() {
-        this.setMovableObject(null);
-    }
+	/**
+	* Removes {@link MovableObject} from the {@link StepableCell}.
+	*/
+	public void stepOff() {
+	this.setMovableObject(null);
+	}
 
-    /**
-     * Indicates whether something currently stands on the cell.
-     * 
-     * @return true if there is some movable object on the cell, otherwise false.
-     */
-    public Boolean isSteppedOn() {
-        return this.getMovableObject() != null;
-    }
+	/**
+	* Indicates whether something currently stands on the cell.
+	* 
+	* @return true if there is some movable object on the cell, otherwise false.
+	*/
+	public Boolean isSteppedOn() {
+	return this.getMovableObject() != null;
+	}
 
-	
-    
-    public MovableObject getMovableObject() {
+
+
+	public MovableObject getMovableObject() {
 		return movableObject;
 	}
 
