430440271b6ea1a0b3bb26d18e265c6bad91e9a8
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-30 13:38:33 +0000

    KeyDoor: implemented boolean open(item)

--- source/group44/entities/KeyDoor.java
+++ source/group44/entities/KeyDoor.java
@@ -1,54 +1,60 @@
 package group44.entities;
-import java.util.ArrayList;
 
+import java.util.ArrayList;
 import group44.game.Level;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+
 /**
- * This class is for the key doors where the player needs a certain coloured 
- * key is needed to be able to open the door. 
- * This class also enherits from the superclass Door.
- * @author Amy Mason
+ * Represents a door for which the player needs a certain coloured key to be
+ * able to open the door.
+ * 
+ * @author Amy Mason, Tomas Svejnoha
  * @version 1.0
  * 
  */
 public class KeyDoor extends Door {
 	private Key.KeyType unlockingKey;
-	private boolean open;
 
 	/**
-	 * This creates a new instance of {@link KeyDoor} and associates it with 
-	 * an unlocking {@link KeyType}.
+	 * This creates a new instance of {@link KeyDoor} and associates it with an
+	 * unlocking {@link KeyType}.
+	 * 
+	 * @param level        - The {@link Level} where the {@link Door} is located
 	 * @param unlockingKey - key used to unlock the door
-	 * @param title - title of the object
-	 * @param positionX - position x in the game
-	 * @param positionY - position y in the game
-	 * @param size - size of the cell on screen
-	 * @param imagePath - Image path of the instance
+	 * @param title        - Title of the {@link Door}
+	 * @param positionX    - Position X in the game
+	 * @param positionY    - Position Y in the game
+	 * @param imagePath    - Path to the Image representing the {@link Door} in the
+	 *                     game
 	 */
-	public KeyDoor (Level level, Key.KeyType unlockingKey, String title, int positionX, int positionY, int size, String imagePath) {
-		super(level, title, positionX, positionY, size, imagePath);
+	public KeyDoor(Level level, Key.KeyType unlockingKey, String title, int positionX, int positionY, int size,
+			String imagePath) {
+		super(level, title, positionX, positionY, imagePath);
 		this.unlockingKey = unlockingKey;
 	}
 
+	/**
+	 * Opens the door if the right Key is used.
+	 * 
+	 * @param item - the Key to use.
+	 * @return true if the door was opened; otherwise false.
+	 */
 	@Override
-	public void open(ArrayList <CollectableItem> items) {
-		if (items != null && !items.isEmpty()) {
-			for(CollectableItem item : items) {
-				//check item is key and not token 
-				if(item instanceof Key) {
-					//casting item so that is thought of as a key and not a collectableItem
-					if (((Key) item).getKeyCode()==unlockingKey.getKeyCode()) {
-						this.open = true;
-					}
-				}
-			}			
+	public boolean open(CollectableItem item) {
+		if (item instanceof Key && ((Key) item).getKeyCode() == this.unlockingKey.getKeyCode()) {
+			this.setIsOpen(true);
 		}
+		return this.isOpen();
 	}
 
+	/**
+	 * Interacts with {@link MovableObject} that stepped on the cell.
+	 * 
+	 * @param object - The {@link MovableObject} that stepped on cell.
+	 */
 	@Override
 	protected void onStepped(MovableObject object) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("KeyDoor.onStepped(object).");
 	}
 }
