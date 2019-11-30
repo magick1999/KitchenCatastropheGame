14a82e41145ba24249271c4413f1cd188d98996e
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-11-30 13:51:59 +0000

    TokenDoor: updated ctor

--- source/group44/entities/TokenDoor.java
+++ source/group44/entities/TokenDoor.java
@@ -6,8 +6,9 @@ import group44.game.Level;
 import javafx.scene.canvas.GraphicsContext;
 
 /**
- * This class is for the token doors where the player needs to 
- * have a certain amount of tokens to be able to open it.
+ * This class is for the token doors where the player needs to have a certain
+ * amount of tokens to be able to open it.
+ * 
  * @author Amy Mason
  * @version 1.0
  * 
@@ -16,16 +17,20 @@ public class TokenDoor extends Door {
 	private int tokensNeeded;
 
 	/**
-	 * This creates a new {@link KeyDoor} and associates a unlocking {@link Key.KeyType} with it. 
+	 * This creates a new {@link KeyDoor} and associates a unlocking
+	 * {@link Key.KeyType} with it.
+	 * 
+	 * @param level        - The {@link Level} where the {@link TokenDoor} is
+	 *                     located.
+	 * @param title        - title of the object.
+	 * @param positionX    - position x in the game.
+	 * @param positionY    - position y in the game.
+	 * @param imagePath    - Path to the Image representing the {@link Door} in the
+	 *                     game.
 	 * @param tokensNeeded - number of tokens needed to open the door.
-	 * @param title - title of the object.
-	 * @param positionX - position x in the game.
-	 * @param positionY - position y in the game.
-	 * @param size - size of the cell on screen.
-	 * @param imagePath - Image path of the instance
 	 */
-	public TokenDoor (Level level, int tokensNeeded, String title, int positionX, int positionY, int size, String imagePath) {
-		super(level, title, positionX, positionY, size, imagePath);
+	public TokenDoor(Level level, String title, int positionX, int positionY, String imagePath, int tokensNeeded) {
+		super(level, title, positionX, positionY, imagePath);
 		this.tokensNeeded = tokensNeeded;
 	}
 	
