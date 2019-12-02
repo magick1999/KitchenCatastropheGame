5e969faa85d1126a29072f53bd817d444728decb
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-02 11:48:57 +0000

    UsernameTakenException: Updated Javadoc

--- source/group44/exceptions/UsernameTakenException.java
+++ source/group44/exceptions/UsernameTakenException.java
@@ -1,7 +1,10 @@
 package group44.exceptions;
 
+import group44.models.Profile;
+
 /**
- * UsernameTakenException
+ * Represents an exception raised when trying to register a new {@link Profile}
+ * with already existing username.
  */
 public class UsernameTakenException extends Exception {
     private static final long serialVersionUID = -2669174231705636936L;
