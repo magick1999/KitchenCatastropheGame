95dcacef934e3c93423f5307da312506498942d4
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-03 22:55:20 +0000

    Level Parsing

---
+++ source/group44/controllers/parsers/LevelObjectParser.java
@@ -0,0 +1,11 @@
+package group44.controllers.parsers;
+
+import java.util.Scanner;
+
+import group44.entities.LevelObject;
+
+public class LevelObjectParser {
+	public static Wall parseWall(Scanner objectScanner) {
+
+	}
+}
