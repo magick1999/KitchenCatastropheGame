5a73a87aa9f58460061b9bc5feab27d3bdfc8691
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-03 19:35:25 +0000

    Update Goal.java

--- source/group44/entities/Goal.java
+++ source/group44/entities/Goal.java
@@ -4,7 +4,7 @@ import group44.game.Level;
 
 /**
  * Represents a Goal in the game.
- * 
+ *
  * @author Tomas Svejnoha
  * @version 1.0
  */
@@ -12,7 +12,7 @@ public class Goal extends StepableCell {
 
     /**
      * Creates a new instance of {@link Goal}.
-     * 
+     *
      * @param level     - The {@link Level} where the {@link Goal} is located
      * @param positionX - Position X of the object in the {@link group44.game.Level}
      * @param positionY - Position Y of the object in the {@link group44.game.Level}
@@ -24,11 +24,11 @@ public class Goal extends StepableCell {
 
     /**
      * Interacts with {@link MovableObject} that stepped on the {@link Goal}.
-     * 
+     *
      * @param object - The {@link MovableObject} that stepped on {@link Goal}.
      */
     @Override
     protected void onStepped(MovableObject object) {
-        this.getLevel().finish();
+        throw new UnsupportedOperationException("GOAL");
     }
 }
