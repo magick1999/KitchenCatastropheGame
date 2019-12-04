215831b77faf3dc038fd1f52f25cddfc9911c20d
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-04 16:17:33 +0000

    TokenDoor: updated stepOn & open & ctor + updated Javadoc

--- source/group44/entities/TokenDoor.java
+++ source/group44/entities/TokenDoor.java
@@ -1,15 +1,16 @@
 package group44.entities;
 
 import group44.entities.CollectableItems.Key;
+import group44.game.CollisionCheckResult;
+import group44.game.CollisionCheckResult.CollisionCheckResultType;
 import group44.game.Level;
 
 /**
  * This class is for the token doors where the player needs to have a certain
  * amount of tokens to be able to open it.
- * 
+ *
  * @author Amy Mason, Tomas Svejnoha
  * @version 1.0
- * 
  */
 public class TokenDoor extends Door {
 	private int tokensNeeded;
@@ -17,40 +18,53 @@ public class TokenDoor extends Door {
 	/**
 	 * This creates a new {@link KeyDoor} and associates a unlocking
 	 * {@link Key.KeyType} with it.
-	 * 
-	 * @param level        - The {@link Level} where the {@link TokenDoor} is
-	 *                     located.
-	 * @param title        - title of the object.
-	 * @param positionX    - position x in the game.
-	 * @param positionY    - position y in the game.
-	 * @param imagePath    - Path to the Image representing the {@link Door} in the
-	 *                     game.
+	 *
+	 * @param level             - The {@link Level} where the {@link KeyDoor} is located.
+	 * @param title             - Title of the {@link Door}.
+	 * @param positionX         - Position X in the game.
+	 * @param positionY         - Position Y in the game.
+	 * @param lockedImagePath   - Path to the Image representing locked door in the game.
+	 * @param unlockedImagePath - Path to the Image representing unlocked door in the game.
 	 * @param tokensNeeded - number of tokens needed to open the door.
 	 */
-	public TokenDoor(Level level, String title, int positionX, int positionY, String imagePath, int tokensNeeded) {
-		super(level, title, positionX, positionY, imagePath);
+	public TokenDoor(Level level, String title, int positionX, int positionY, String lockedImagePath, String unlockedImagePath, int tokensNeeded) {
+		super(level, title, positionX, positionY, lockedImagePath, unlockedImagePath);
+
 		this.tokensNeeded = tokensNeeded;
 	}
 
 	/**
-	 * Opens the door if a {@link TokenAccumulator} with the right amount of tokens
-	 * were used.
-	 * 
+	 * Opens the door if a {@link TokenAccumulator} with the right amount of tokens were used.
+	 *
 	 * @param item - the TokenAccumulator to use.
 	 * @return true if the door was opened; otherwise false.
 	 */
 	@Override
 	public boolean open(CollectableItem item) {
-		if (this.isOpen() == false && item instanceof TokenAccumulator
-				&& ((TokenAccumulator) item).getTokensCount() >= this.tokensNeeded) {
+		if (this.isOpen() == false && item instanceof TokenAccumulator && ((TokenAccumulator) item).getTokensCount() >= this.tokensNeeded) {
 			try {
 				((TokenAccumulator) item).use(this.tokensNeeded);
-				this.setIsOpen(true);
+				this.open();
 			} catch (Exception e) {
-				// DO nothing
+				return false;
 			}
 		}
 
 		return this.isOpen();
+	}
+
+	/**
+     * Places {@link MovableObject} on the {@link TokenDoor}.
+     *
+     * @param object - {@link MovableObject} that steps on the cell.
+     *
+     * @return the {@link CollisionCheckResult} with information about the action result.
+     */
+	@Override
+	public CollisionCheckResult stepOn(MovableObject object) {
+		if (this.isOpen() == false) {
+			return new CollisionCheckResult(CollisionCheckResultType.NotEnoughTokens, this);
+		}
+		return new CollisionCheckResult(CollisionCheckResultType.Successful);
 	}
 }
