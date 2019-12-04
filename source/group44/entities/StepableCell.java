11832aaffedf4ef03f331259b8e118de76c243af
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-04 14:39:40 +0000

    StepableCell: made stopOn(obj) abstract + Updated Javadoc

--- source/group44/entities/StepableCell.java
+++ source/group44/entities/StepableCell.java
@@ -2,10 +2,11 @@ package group44.entities;
 
 import group44.game.CollisionCheckResult;
 import group44.game.Level;
+import group44.game.CollisionCheckResult.CollisionCheckResultType;
 
 /**
  * An abstract class for cells on which can {@link MovableObject} step.
- * 
+ *
  * @author Tomas Svejnoha
  * @version 1.0
  */
@@ -14,12 +15,12 @@ public abstract class StepableCell extends Cell {
 
     /**
      * Creates a new {@link StepableCell}.
-     * 
-     * @param level     - The {@link Level} where the object is located
-     * @param title     - Title of the object
-     * @param positionX - Position X in the game
-     * @param positionY - Position Y in the game
-     * @param imagePath - Image path of the instance
+     *
+     * @param level     - The {@link Level} where the object is located.
+     * @param title     - Title of the object.
+     * @param positionX - Position X in the game.
+     * @param positionY - Position Y in the game.
+     * @param imagePath - Image path of the instance.
      */
     public StepableCell(Level level, String title, int positionX, int positionY, String imagePath) {
         super(level, title, positionX, positionY, imagePath);
@@ -30,21 +31,13 @@ public abstract class StepableCell extends Cell {
      * {@link CollisionCheckResult}. If there is already some {@link MovableObject}
      * on the {@link StepableCell}, a {@link CollisionCheckResult} with colliding
      * object is returned.
-     * 
-     * @param object - {@link MovableObject} that steps on the cell
-     * 
-     * @return the {@link CollisionCheckResult} with the colliding
-     *         {@link MovableObject}
+     *
+     * @param object - {@link MovableObject} that steps on the cell.
+     *
+     * @return the {@link CollisionCheckResult} with a result and colliding
+     *         {@link LevelObject}.
      */
-    public CollisionCheckResult stepOn(MovableObject object) {
-        if (this.movableObject == null) {
-            this.movableObject = object;
-            this.onStepped(object);
-            return new CollisionCheckResult(null);
-        } else {
-            return new CollisionCheckResult(this.movableObject);
-        }
-    }
+    public abstract CollisionCheckResult stepOn(MovableObject object);
 
     /**
      * Removes {@link MovableObject} from the {@link StepableCell}.
@@ -55,7 +48,7 @@ public abstract class StepableCell extends Cell {
 
     /**
      * Indicates whether something currently stands on the cell.
-     * 
+     *
      * @return true if there is some movable object on the cell, otherwise false.
      */
     public Boolean isSteppedOn() {
@@ -64,8 +57,8 @@ public abstract class StepableCell extends Cell {
 
     /**
      * Returns the {@link MovableObject} which stepped on the {@link StepableCell}.
-     * 
-     * @return the {@link MovableObject}
+     *
+     * @return the {@link MovableObject}.
      */
     public MovableObject getMovableObject() {
         return this.movableObject;
@@ -73,7 +66,7 @@ public abstract class StepableCell extends Cell {
 
     /**
      * Interacts with {@link MovableObject} that stepped on the cell.
-     * 
+     *
      * @param object - The {@link MovableObject} that stepped on cell.
      */
     protected abstract void onStepped(MovableObject object);
