bc3f667f0daf3124cf08b742da892ccc0af44e78
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-05 19:55:35 +0000

    Constants: Formated

--- source/group44/Constants.java
+++ source/group44/Constants.java
@@ -1,37 +1,45 @@
 package group44;
 
 public final class Constants {
-    private Constants(){}
-    public static final int WINDOW_WIDTH = 800;
-    public static final int WINDOW_HEIGHT = 600;
+	private Constants() {
+	}
 
-    // The dimensions of the canvas
-    public static final int CANVAS_WIDTH = 725;
-    public static final int CANVAS_HEIGHT = 550;
+	// Data - Folders
+	public static final String FOLDER_LEVELS = "group44/data/levels/";
 
-    // The size of each cell
-    public static int GRID_CELL_WIDTH = 60;
-    public static int GRID_CELL_HEIGHT = 60;
+	// Data - Files
+	public static final String FILE_RECORDS = "group44/data/records.txt";
+	public static final String FILE_PROFILES = "group44/data/profiles.txt";
 
-    // Drawing constants
-    public static int LEVEL_DISPLAY_SIZE = 7; // Must be odd and greater or equal 3
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 600;
 
-    // Parsing constants
-    public static final String TYPE_WALL = "wall";
-    public static final String TYPE_GROUND = "ground";
-    public static final String TYPE_WATER = "water";
-    public static final String TYPE_FIRE = "fire";
-    public static final String TYPE_GOAL = "goal";
-    public static final String TYPE_PLAYER = "player";
-    public static final String TYPE_FIRE_BOOTS = "fireBoots";
-    public static final String TYPE_FLIPPERS = "flippers";
-    public static final String TYPE_KEY = "key";
-    public static final String TYPE_TOKEN = "token";
-    public static final String TYPE_DUMB_TARGETING_ENEMY = "dumbTargetingEnemy";
-    public static final String TYPE_STRAIGHT_WALKING_ENEMY = "straightWalkingEnemy";
-    public static final String TYPE_WALL_FOLLOWING_ENEMY = "wallFollowingEnemy";
-    public static final String TYPE_TOKEN_DOOR = "tokenDoor";
-    public static final String TYPE_KEY_DOOR = "keyDoor";
-    
-    
+	// The dimensions of the canvas
+	public static final int CANVAS_WIDTH = 725;
+	public static final int CANVAS_HEIGHT = 550;
+
+	// The size of each cell
+	public static int GRID_CELL_WIDTH = 60;
+	public static int GRID_CELL_HEIGHT = 60;
+
+	// Drawing constants
+	public static int LEVEL_DISPLAY_SIZE = 7; // Must be odd and greater or
+												// equal 3
+
+	// Parsing constants
+	public static final String TYPE_WALL = "wall";
+	public static final String TYPE_GROUND = "ground";
+	public static final String TYPE_WATER = "water";
+	public static final String TYPE_FIRE = "fire";
+	public static final String TYPE_GOAL = "goal";
+	public static final String TYPE_PLAYER = "player";
+	public static final String TYPE_FIRE_BOOTS = "fireBoots";
+	public static final String TYPE_FLIPPERS = "flippers";
+	public static final String TYPE_KEY = "key";
+	public static final String TYPE_TOKEN = "token";
+	public static final String TYPE_DUMB_TARGETING_ENEMY = "dumbTargetingEnemy";
+	public static final String TYPE_STRAIGHT_WALKING_ENEMY = "straightWalkingEnemy";
+	public static final String TYPE_WALL_FOLLOWING_ENEMY = "wallFollowingEnemy";
+	public static final String TYPE_TOKEN_DOOR = "tokenDoor";
+	public static final String TYPE_KEY_DOOR = "keyDoor";
 }
