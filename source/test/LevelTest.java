8a444bf89d7cabc06c9f3347c7bd67aee771607e
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-05 11:22:35 +0000

    LevelTest: updated test generating

--- source/test/LevelTest.java
+++ source/test/LevelTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 import group44.Constants;
 import group44.controllers.LevelManager;
@@ -10,6 +12,7 @@ import group44.exceptions.CollisionException;
 import group44.exceptions.ParsingException;
 import group44.game.Level;
 import group44.models.LevelInfo;
+import javafx.embed.swing.JFXPanel;
 
 public class LevelTest {
 	private static int DISPLAY_SIZE = 7;
@@ -22,15 +25,22 @@ public class LevelTest {
 	private static String PARSE_PATTERN_CELL = "%s,%d,%d,%s";
 	private static String PARSE_PATTERN_PLAYER = "%s,%s,%d,%d,%d,%d,%s";
 
-	private static String PATH_IMAGE_WALL = "group44/resources/WallCounter.png";
-	private static String PATH_IMAGE_GROUND = "group44/resources/cells/floor.png";
-	private static String PATH_IMAGE_WATER = "group44/resources/cells/water.png";
-	private static String PATH_IMAGE_FIRE = "group44/resources/cells/fire.png";
-	private static String PATH_IMAGE_GOAL = "group44/resources/cells/goal.png";
-	private static String PATH_IMAGE_PLAYER = "group44/resources/ChefDownWalk/Front1.png";
+	private static String BASE_PATH_IMAGE = "source/group44/resources/";
 
-	public static void main(String[] args) {
-		generateLevelFile01();
+	private static String PATH_IMAGE_WALL = BASE_PATH_IMAGE + "WallCounter.png";
+	private static String PATH_IMAGE_GROUND = BASE_PATH_IMAGE + "cells/floor.png";
+	private static String PATH_IMAGE_WATER = BASE_PATH_IMAGE + "cells/water.png";
+	private static String PATH_IMAGE_FIRE = BASE_PATH_IMAGE + "cells/fire.png";
+	private static String PATH_IMAGE_GOAL = BASE_PATH_IMAGE + "cells/goal.png";
+	private static String PATH_IMAGE_PLAYER = BASE_PATH_IMAGE + "ChefDownWalk/Front1.png";
+
+	public static void main(String[] args) throws FileNotFoundException {
+		JFXPanel jfxPanel = new JFXPanel();
+		generateLevelFile01("source/group44/data/levels/level_001.txt");
+
+		//File f = new File(PATH_IMAGE_GROUND);
+		//System.out.println(f.exists());
+		//System.out.println(f.getAbsolutePath());
 
 		if (true) {
 			LevelManager.load(LEVELS);
@@ -38,7 +48,8 @@ public class LevelTest {
 			for (LevelInfo info : LevelManager.getLevelInfos()) {
 				if (info.getId() == 1) {
 					try {
-						Level level = LevelParser.parseLevel(info);
+						Level level = LevelManager.load(info);
+						System.out.println(level.getId());
 					} catch (FileNotFoundException | CollisionException | ParsingException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -53,43 +64,45 @@ public class LevelTest {
 		Level level = new Level(id, width, height, DISPLAY_SIZE);
 	}
 
-	public static void generateLevelFile01() {
+	public static void generateLevelFile01(String path) throws FileNotFoundException {
 		int id = 1;
 		int width = 20;
 		int height = 20;
 
-		System.out.println(String.format("%d,%d,%d", id, width, height));
+		PrintWriter writer = new PrintWriter(path);
+
+		writer.println(String.format("%d,%d,%d", id, width, height));
 
 		// TOP BORDER
 		for (int x = 0; x < width; x++) {
-			System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, x, 0, PATH_IMAGE_WALL));
+			writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, x, 0, PATH_IMAGE_WALL));
 		}
 
 		// BOTTOM BORDER
 		for (int x = 0; x < width; x++) {
-			System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, x, height - 1, PATH_IMAGE_WALL));
+			writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, x, height - 1, PATH_IMAGE_WALL));
 		}
 
 		// LEFT BORDER
 		for (int y = 1; y < height - 1; y++) {
-			System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, 0, y, PATH_IMAGE_WALL));
+			writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, 0, y, PATH_IMAGE_WALL));
 		}
 
 		// RIGHT BORDER
 		for (int y = 1; y < height - 1; y++) {
-			System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, width - 1, y, PATH_IMAGE_WALL));
+			writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WALL, width - 1, y, PATH_IMAGE_WALL));
 		}
 
 		// GROUND [1,1]-[3,18]
 		for (int x = 1; x < 4; x++) {
 			for (int y = 1; y < height - 2; y++) {
 				if (x == 1 && y == 1) {
-					System.out.print(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
-					System.out.println(String.format("," + PARSE_PATTERN_PLAYER, Constants.TYPE_PLAYER, PLAYER_NAME, x, y,
+					writer.print(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
+					writer.println(String.format("," + PARSE_PATTERN_PLAYER, Constants.TYPE_PLAYER, PLAYER_NAME, x, y,
 							PLAYER_VECTOR_X, PLAYER_VECTOR_Y, PATH_IMAGE_PLAYER));
 					continue;
 				}
-				System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
+				writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
 			}
 		}
 
@@ -97,12 +110,13 @@ public class LevelTest {
 		for (int x = 4; x < 7; x++) {
 			for (int y = 1; y < height - 2; y++) {
 				if (y == 9) {
-					System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
+					writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
+					continue;
 				}
 				if (x == 5) {
-					System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_FIRE, x, y, PATH_IMAGE_FIRE));
+					writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_FIRE, x, y, PATH_IMAGE_FIRE));
 				} else {
-					System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WATER, x, y, PATH_IMAGE_WATER));
+					writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_WATER, x, y, PATH_IMAGE_WATER));
 				}
 			}
 		}
@@ -112,11 +126,13 @@ public class LevelTest {
 			for (int y = 1; y < height - 1; y++) {
 				// GOAL
 				if (x == 18 && y == 18) {
-					System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GOAL, x, y, PATH_IMAGE_GOAL));
+					writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GOAL, x, y, PATH_IMAGE_GOAL));
 					continue;
 				}
-				System.out.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
+				writer.println(String.format(PARSE_PATTERN_CELL, Constants.TYPE_GROUND, x, y, PATH_IMAGE_GROUND));
 			}
 		}
+
+		writer.close();
 	}
 }
