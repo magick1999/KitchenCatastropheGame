f1f83f1cb0819a40715fe773431c5032b136c4e7
mason0109 <mason.mason0109@gmail.com>
2019-12-05 16:16:50 +0000

    Parser updated for Collectable Items

--- source/group44/controllers/parsers/LevelParser.java
+++ source/group44/controllers/parsers/LevelParser.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 
 import group44.Constants;
 import group44.entities.cells.*;
+import group44.entities.collectableItems.CollectableItem;
+import group44.entities.collectableItems.FireBoots;
+import group44.entities.collectableItems.Flippers;
+import group44.entities.collectableItems.Key;
+import group44.entities.collectableItems.Key.KeyType;
+import group44.entities.collectableItems.Token;
 import group44.entities.movableObjects.*;
 import group44.exceptions.CollisionException;
 import group44.exceptions.ParsingException;
@@ -14,7 +20,7 @@ import group44.models.LevelInfo;
 /**
  * Parses the level file into {@link Level}.
  *
- * @author Tomas Svejnoha
+ * @author Tomas Svejnoha, Amy Mason
  * @version 1.0
  */
 public class LevelParser {
@@ -24,8 +30,8 @@ public class LevelParser {
 	 * @param info - the {@link LevelInfo}.
 	 * @return the parsed {@link Level}.
 	 *
-	 * @throws CollisionException when two cells are at the same position.
-	 * @throws ParsingException when trying to parse invalid data type.
+	 * @throws CollisionException    when two cells are at the same position.
+	 * @throws ParsingException      when trying to parse invalid data type.
 	 * @throws FileNotFoundException when level file is not found.
 	 */
 	public static Level parseLevel(LevelInfo info) throws CollisionException, ParsingException, FileNotFoundException {
@@ -58,11 +64,11 @@ public class LevelParser {
 	/**
 	 * Parses level file and adds LevelObjects into level.
 	 *
-	 * @param level - the level to parse.
+	 * @param level       - the level to parse.
 	 * @param fileScanner - scanner for the level file.
 	 *
 	 * @throws CollisionException when two cells are at the same position.
-	 * @throws ParsingException when trying to parse invalid data type.
+	 * @throws ParsingException   when trying to parse invalid data type.
 	 */
 	private static void parseLevel(Level level, Scanner fileScanner) throws CollisionException, ParsingException {
 		while (fileScanner.hasNextLine()) {
@@ -74,7 +80,7 @@ public class LevelParser {
 	/**
 	 * Parses Cell on the line.
 	 *
-	 * @param level - the level where the Cell is located.
+	 * @param level   - the level where the Cell is located.
 	 * @param scanner - scanner with the serialised cell.
 	 *
 	 * @return the parsed {@link Cell}
@@ -104,7 +110,7 @@ public class LevelParser {
 	/**
 	 * Parses the line into cell.
 	 *
-	 * @param level - the level where the Cell is located.
+	 * @param level   - the level where the Cell is located.
 	 * @param scanner - scanner with the serialised cell.
 	 *
 	 * @return the serialised {@link Wall} as a type {@link Cell}.
@@ -120,7 +126,7 @@ public class LevelParser {
 	/**
 	 * Parses the line into cell.
 	 *
-	 * @param level - the level where the Cell is located.
+	 * @param level   - the level where the Cell is located.
 	 * @param scanner - scanner with the serialised cell.
 	 *
 	 * @return the serialised {@link Ground} as a type {@link Cell}.
@@ -130,17 +136,28 @@ public class LevelParser {
 		int positionY = scanner.nextInt();
 		String imagePath = scanner.next();
 		MovableObject movableObject = null;
+		CollectableItem collectableItem = null;
 
 		if (scanner.hasNext()) {
 			String type = scanner.next();
 
 			if (type.equals(Constants.TYPE_PLAYER)) {
 				movableObject = parsePlayerEntry(level, scanner);
+			} else if (type.equals(Constants.TYPE_FIRE_BOOTS)) {
+				collectableItem = parseFireBootsEntry(level, scanner);
+			} else if (type.equals(Constants.TYPE_FLIPPERS)) {
+				collectableItem = parseFlipperEntry(level, scanner);
+			} else if (type.equals(Constants.TYPE_KEY)) {
+				collectableItem = parseKeyEntry(level, scanner);
+			} else if (type.equals(Constants.TYPE_TOKEN)) {
+				collectableItem = parseTokenEntry(level, scanner);
 			}
 		}
 
 		if (movableObject != null) {
 			return new Ground(level, positionX, positionY, imagePath, movableObject);
+		} else if (collectableItem != null) {
+			return new Ground(level, positionX, positionY, imagePath, collectableItem);
 		} else {
 			return new Ground(level, positionX, positionY, imagePath);
 		}
@@ -149,7 +166,7 @@ public class LevelParser {
 	/**
 	 * Parses the line into cell.
 	 *
-	 * @param level - the level where the Cell is located.
+	 * @param level   - the level where the Cell is located.
 	 * @param scanner - scanner with the serialised cell.
 	 *
 	 * @return the serialised {@link Water} as a type {@link Cell}.
@@ -165,7 +182,7 @@ public class LevelParser {
 	/**
 	 * Parses the line into cell.
 	 *
-	 * @param level - the level where the Cell is located.
+	 * @param level   - the level where the Cell is located.
 	 * @param scanner - scanner with the serialised cell.
 	 *
 	 * @return the serialised {@link Fire} as a type {@link Cell}.
@@ -181,7 +198,7 @@ public class LevelParser {
 	/**
 	 * Parses the line into cell.
 	 *
-	 * @param level - the level where the Cell is located.
+	 * @param level   - the level where the Cell is located.
 	 * @param scanner - scanner with the serialised cell.
 	 *
 	 * @return the serialised {@link Goal} as a type {@link Cell}.
@@ -197,7 +214,7 @@ public class LevelParser {
 	/**
 	 * Parses the player on the scanned line.
 	 *
-	 * @param level - the level where the player is located.
+	 * @param level   - the level where the player is located.
 	 * @param scanner - scanner with the serialised player.
 	 *
 	 * @return the serialised {@link Player} as a type of {@link MovableObject}.
@@ -211,5 +228,88 @@ public class LevelParser {
 		String imagePath = scanner.next();
 
 		return new Player(level, name, positionX, positionY, velocityX, velocityY, imagePath);
+	}
+
+	/**
+	 * Parses the line into Collectable item - Fire Boots.
+	 * 
+	 * @param level   - the level where the item is located.
+	 * @param scanner - scanner with the serialised player.
+	 * 
+	 * @return the serialised {@link FireBoots} as a type {@link CollectableItem}.
+	 */
+	private static CollectableItem parseFireBootsEntry(Level level, Scanner scanner) {
+		int positionX = scanner.nextInt();
+		int positionY = scanner.nextInt();
+		String imagePath = scanner.next();
+
+		return new FireBoots(level, positionX, positionY, imagePath);
+	}
+
+	/**
+	 * Parses the line into Collectable item - Flippers.
+	 * 
+	 * @param level   - the level where the item is located.
+	 * @param scanner - scanner with the serialised player.
+	 * 
+	 * @return the serialised {@link Flippers} as a type {@link CollectableItem}.
+	 */
+	private static CollectableItem parseFlipperEntry(Level level, Scanner scanner) {
+		int positionX = scanner.nextInt();
+		int positionY = scanner.nextInt();
+		String imagePath = scanner.next();
+
+		return new Flippers(level, positionX, positionY, imagePath);
+	}
+
+	/**
+	 * Parses the line into Collectable item - Key.
+	 * 
+	 * @param level   - the level where the item is located.
+	 * @param scanner - scanner with the serialised player.
+	 * 
+	 * @return the serialised {@link Key} as a type {@link CollectableItem}.
+	 */
+	private static CollectableItem parseKeyEntry(Level level, Scanner scanner) {
+		int keyTypeID = scanner.nextInt();
+		int positionX = scanner.nextInt();
+		int positionY = scanner.nextInt();
+
+		KeyType keyType = null;
+		switch (keyTypeID) {
+		case 1:
+			keyType = KeyType.RED;
+			break;
+		case 2:
+			keyType = KeyType.BLUE;
+			break;
+		case 3:
+			keyType = KeyType.GREEN;
+			break;
+		case 4:
+			keyType = KeyType.GOLD;
+			break;
+		default:
+			keyType = KeyType.RED;
+			break;
+		}
+
+		return new Key(level, keyType, positionX, positionY);
+	}
+
+	/**
+	 * Parses the line into Collectable item - Token.
+	 * 
+	 * @param level   - the level where the item is located.
+	 * @param scanner - scanner with the serialised player.
+	 * 
+	 * @return the serialised {@link Token} as a type {@link CollectableItem}.
+	 */
+	private static CollectableItem parseTokenEntry(Level level, Scanner scanner) {
+		int positionX = scanner.nextInt();
+		int positionY = scanner.nextInt();
+		String imagePath = scanner.next();
+
+		return new Token(level, positionX, positionY, imagePath);
 	}
 }
