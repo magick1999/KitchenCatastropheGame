e27cfb5566c98d3e20ca4c1729912a0b53889073
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-05 15:58:24 +0000

    Code formating

--- source/group44/controllers/Leaderboard.java
+++ source/group44/controllers/Leaderboard.java
@@ -19,206 +19,219 @@ import javafx.collections.ObservableList;
  * @version 1.0
  */
 public class Leaderboard {
-    private static ObservableList<Record> records = FXCollections.observableArrayList(new ArrayList<Record>());
+	private static ObservableList<Record> records = FXCollections.observableArrayList(new ArrayList<Record>());
 
-    /**
-     * Returns an {@link ObservableList} of records.
-     *
-     * @return profiles
-     */
-    public static ObservableList<Record> getProfiles() {
-    	return Leaderboard.records;
-    }
+	/**
+	 * Returns an {@link ObservableList} of records.
+	 *
+	 * @return profiles
+	 */
+	public static ObservableList<Record> getProfiles() {
+		return Leaderboard.records;
+	}
 
-    /**
-     * Adds or updates a record in the {@link Leaderboard} if the time is better.
-     *
-     * @param profile - {@link Profile} of the user
-     * @param levelId - Id of the {@link Level} the user finished
-     * @param time    - time taken to finish the {@link Level}
-     */
-    public static void addOrUpdate(Profile profile, int levelId, long time) {
-        Record record = null;
+	/**
+	 * Adds or updates a record in the {@link Leaderboard} if the time is
+	 * better.
+	 *
+	 * @param profile
+	 *            - {@link Profile} of the user
+	 * @param levelId
+	 *            - Id of the {@link Level} the user finished
+	 * @param time
+	 *            - time taken to finish the {@link Level}
+	 */
+	public static void addOrUpdate(Profile profile, int levelId, long time) {
+		Record record = null;
 
-        for (Record item : Leaderboard.records) {
-            if (item.getProfile().getId() == profile.getId() && item.getLevelId() == levelId) {
-                record = item;
-                break;
-            }
-        }
-
-        if (record == null) {
-            Leaderboard.records.add(new Record(profile, levelId, time));
-        } else if (record.getTime() > time) {
-            record.setTime(time);
-        }
-    }
-
-    /**
-     * Returns up to 3 top records for the level.
-     *
-     * @param levelId - id of the {@link Level} for which we want the records
-     * @return array of up to 3 top records
-     */
-    public static Record[] getTopThreeRecords(int levelId) {
-        ArrayList<Record> levelRecords = new ArrayList<>();
-
-        for (Record item : Leaderboard.records) {
-            if (item.getLevelId() == levelId) {
-                levelRecords.add(item);
-            }
-        }
-
-        Collections.sort(levelRecords);
-
-        Record[] top3 = new Record[(levelRecords.size() < 3) ? levelRecords.size() : 3];
-        for (int i = 0; i < 3; i++) {
-            top3[i] = levelRecords.get(i);
-        }
-
-        return top3;
-    }
-
-    /**
-     * Indicates whether the used scored in top 3 for a specific level.
-     *
-     * @param profile - the user {@link Profile}.
-     * @param levelId - the level id.
-     * @return true if user is in top 3; otherwise false.
-     */
-    public static boolean isInTopThreeRecors(Profile profile, int levelId) {
-        Record[] records = Leaderboard.getTopThreeRecords(levelId);
-        for (Record item : records) {
-            if (item.getProfile().getId() == profile.getId()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Returns an id the highest {@link Level} achieved by user.
-     *
-     * @param profile - profile of the user.
-     * @return the id of the highest level; 0 if user did not finish any level.
-     */
-    public static int getAchievedLevel(Profile profile) {
-        int level = 0;
-
-        for (Record record : Leaderboard.records) {
-            if (record.getProfile() == profile) {
-                if (level < record.getLevelId()) {
-                    level = record.getLevelId();
-                }
-            }
-        }
-
-        return level;
-    }
-
-    /**
-     * Loads records from the file.
-     *
-     * @param path - path to a file with records
-     */
-    public static void load(String path) {
-        ArrayList<Record> loadedRecords = null;
-        Scanner fileScanner = null;
-        File file = new File(path);
-
-        try {
-            fileScanner = new Scanner(file);
-            loadedRecords = Leaderboard.load(fileScanner);
-        } catch (Exception e) {
-            System.out.println("Failed to load records from file (" + path + ").");
-            loadedRecords = new ArrayList<Record>();
-        } finally {
-            if (fileScanner != null) {
-                fileScanner.close();
-            }
-            for (Record record : loadedRecords) {
-            	Leaderboard.records.add(record);
+		for (Record item : Leaderboard.records) {
+			if (item.getProfile().getId() == profile.getId() && item.getLevelId() == levelId) {
+				record = item;
+				break;
 			}
-        }
-    }
+		}
 
-    /**
-     * Loads stored records.
-     *
-     * @param fileScanner - scanner of the file where the records are stored
-     * @return a list of loaded {@link Record}s
-     */
-    private static ArrayList<Record> load(Scanner fileScanner) {
-        ArrayList<Record> loadedRecords = new ArrayList<>();
+		if (record == null) {
+			Leaderboard.records.add(new Record(profile, levelId, time));
+		} else if (record.getTime() > time) {
+			record.setTime(time);
+		}
+	}
 
-        while (fileScanner.hasNextLine()) {
-            Record r = Leaderboard.parseRecord(new Scanner(fileScanner.nextLine()));
-            if (r != null) {
-                loadedRecords.add(r);
-            }
-        }
+	/**
+	 * Returns up to 3 top records for the level.
+	 *
+	 * @param levelId
+	 *            - id of the {@link Level} for which we want the records
+	 * @return array of up to 3 top records
+	 */
+	public static Record[] getTopThreeRecords(int levelId) {
+		ArrayList<Record> levelRecords = new ArrayList<>();
 
-        return loadedRecords;
-    }
+		for (Record item : Leaderboard.records) {
+			if (item.getLevelId() == levelId) {
+				levelRecords.add(item);
+			}
+		}
 
-    /**
-     * Creates a new {@link Record} from the Scanner.
-     *
-     * @param scanner - scanner with the serialised record
-     * @return created {@link Record}; null if associated {@link Profile} was not
-     *         found
-     */
-    private static Record parseRecord(Scanner scanner) {
-        Record record = null;
-        scanner.useDelimiter(",");
+		Collections.sort(levelRecords);
 
-        try {
-            int profileId = scanner.nextInt();
-            int levelId = scanner.nextInt();
-            long time = scanner.nextLong();
+		Record[] top3 = new Record[(levelRecords.size() < 3) ? levelRecords.size() : 3];
+		for (int i = 0; i < 3; i++) {
+			top3[i] = levelRecords.get(i);
+		}
 
-            Profile profile = ProfileManager.getProfile(profileId);
-            if (profile != null) {
-                record = new Record(profile, levelId, time);
-            } else {
-                System.out.println("Unable to parse a Record. Profile(id=" + profileId + ") not found!");
-            }
-        } catch (Exception e) {
-            System.out.println("Unable to parse a Profile.\n" + e.getMessage());
-        }
+		return top3;
+	}
 
-        return record;
-    }
+	/**
+	 * Indicates whether the used scored in top 3 for a specific level.
+	 *
+	 * @param profile
+	 *            - the user {@link Profile}.
+	 * @param levelId
+	 *            - the level id.
+	 * @return true if user is in top 3; otherwise false.
+	 */
+	public static boolean isInTopThreeRecors(Profile profile, int levelId) {
+		Record[] records = Leaderboard.getTopThreeRecords(levelId);
+		for (Record item : records) {
+			if (item.getProfile().getId() == profile.getId()) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Saves all records in the {@link Leaderboard}.
-     *
-     * @param path - path to the file where to store the profiles
-     */
-    public static void save(String path) {
-        File file = new File(path);
-        PrintWriter writer = null;
+	/**
+	 * Returns an id the highest {@link Level} achieved by user.
+	 *
+	 * @param profile
+	 *            - profile of the user.
+	 * @return the id of the highest level; 0 if user did not finish any level.
+	 */
+	public static int getAchievedLevel(Profile profile) {
+		int level = 0;
 
-        try {
-            writer = new PrintWriter(file);
-            Leaderboard.save(writer);
-        } catch (Exception e) {
-            System.out.println("Unable to save records.\n" + e.getMessage());
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
-    }
+		for (Record record : Leaderboard.records) {
+			if (record.getProfile() == profile) {
+				if (level < record.getLevelId()) {
+					level = record.getLevelId();
+				}
+			}
+		}
 
-    /**
-     * Saves records using provided writer.
-     *
-     * @param writer - {@link PrintWriter} to use when saving r cords
-     */
-    private static void save(PrintWriter writer) {
-        for (Record record : Leaderboard.records) {
-            writer.println(record.toString());
-        }
-    }
+		return level;
+	}
+
+	/**
+	 * Loads records from the file.
+	 *
+	 * @param path
+	 *            - path to a file with records
+	 */
+	public static void load(String path) {
+		ArrayList<Record> loadedRecords = null;
+		Scanner fileScanner = null;
+		File file = new File(path);
+
+		try {
+			fileScanner = new Scanner(file);
+			loadedRecords = Leaderboard.load(fileScanner);
+		} catch (Exception e) {
+			System.out.println("Failed to load records from file (" + path + ").");
+			loadedRecords = new ArrayList<Record>();
+		} finally {
+			if (fileScanner != null) {
+				fileScanner.close();
+			}
+			for (Record item : loadedRecords) {
+				Leaderboard.records.add(item);
+			}
+		}
+	}
+
+	/**
+	 * Loads stored records.
+	 *
+	 * @param fileScanner
+	 *            - scanner of the file where the records are stored
+	 * @return a list of loaded {@link Record}s
+	 */
+	private static ArrayList<Record> load(Scanner fileScanner) {
+		ArrayList<Record> loadedRecords = new ArrayList<>();
+
+		while (fileScanner.hasNextLine()) {
+			Record r = Leaderboard.parseRecord(new Scanner(fileScanner.nextLine()));
+			if (r != null) {
+				loadedRecords.add(r);
+			}
+		}
+
+		return loadedRecords;
+	}
+
+	/**
+	 * Creates a new {@link Record} from the Scanner.
+	 *
+	 * @param scanner
+	 *            - scanner with the serialised record
+	 * @return created {@link Record}; null if associated {@link Profile} was
+	 *         not found
+	 */
+	private static Record parseRecord(Scanner scanner) {
+		Record record = null;
+		scanner.useDelimiter(",");
+
+		try {
+			int profileId = scanner.nextInt();
+			int levelId = scanner.nextInt();
+			long time = scanner.nextLong();
+
+			Profile profile = ProfileManager.getProfile(profileId);
+			if (profile != null) {
+				record = new Record(profile, levelId, time);
+			} else {
+				System.out.println("Unable to parse a Record. Profile(id=" + profileId + ") not found!");
+			}
+		} catch (Exception e) {
+			System.out.println("Unable to parse a Profile.\n" + e.getMessage());
+		}
+
+		return record;
+	}
+
+	/**
+	 * Saves all records in the {@link Leaderboard}.
+	 *
+	 * @param path
+	 *            - path to the file where to store the profiles
+	 */
+	public static void save(String path) {
+		File file = new File(path);
+		PrintWriter writer = null;
+
+		try {
+			writer = new PrintWriter(file);
+			Leaderboard.save(writer);
+		} catch (Exception e) {
+			System.out.println("Unable to save records.\n" + e.getMessage());
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
+		}
+	}
+
+	/**
+	 * Saves records using provided writer.
+	 *
+	 * @param writer
+	 *            - {@link PrintWriter} to use when saving r cords
+	 */
+	private static void save(PrintWriter writer) {
+		for (Record record : Leaderboard.records) {
+			writer.println(record.toString());
+		}
+	}
 }
--- source/group44/controllers/LevelManager.java
+++ source/group44/controllers/LevelManager.java
@@ -18,99 +18,107 @@ import group44.game.Level;
  * @version 1.0
  */
 public class LevelManager {
-    private static final String LEVEL_FILE_PATTERN = "^level_[0-9]+\\.txt$";
-    private static ArrayList<LevelInfo> levelInfos = new ArrayList<>();
+	private static final String LEVEL_FILE_PATTERN = "^level_[0-9]+\\.txt$";
+	private static ArrayList<LevelInfo> levelInfos = new ArrayList<>();
 
-    /**
-     * Returns information about available levels.
-     *
-     * @return A list of {@link LevelInfo}
-     */
-    public static ArrayList<LevelInfo> getLevelInfos() {
-        return LevelManager.levelInfos;
-    }
+	/**
+	 * Returns information about available levels.
+	 *
+	 * @return A list of {@link LevelInfo}
+	 */
+	public static ArrayList<LevelInfo> getLevelInfos() {
+		return LevelManager.levelInfos;
+	}
 
-    /**
-     * Loads metadata about levels in a folder.
-     *
-     * @param directory - directory containing all {@link Level} files
-     */
-    public static void load(String directory) {
-        for (File item : LevelManager.getLevelFiles(new File(directory))) {
-            LevelInfo info = LevelManager.getLevelInfo(item);
-            if (info != null) {
-                LevelManager.levelInfos.add(info);
-            }
-        }
-    }
+	/**
+	 * Loads metadata about levels in a folder.
+	 *
+	 * @param directory
+	 *            - directory containing all {@link Level} files
+	 */
+	public static void load(String directory) {
+		for (File item : LevelManager.getLevelFiles(new File(directory))) {
+			LevelInfo info = LevelManager.getLevelInfo(item);
+			if (info != null) {
+				LevelManager.levelInfos.add(info);
+			}
+		}
+	}
 
-    /**
-     * Returns a list of files containing definitions of {@link Level}s.
-     *
-     * @param directory - the directory with {@link Level}s
-     * @return the list of {@link Level} files
-     */
-    private static ArrayList<File> getLevelFiles(File directory) {
-        ArrayList<File> levelFiles = new ArrayList<>();
+	/**
+	 * Returns a list of files containing definitions of {@link Level}s.
+	 *
+	 * @param directory
+	 *            - the directory with {@link Level}s
+	 * @return the list of {@link Level} files
+	 */
+	private static ArrayList<File> getLevelFiles(File directory) {
+		ArrayList<File> levelFiles = new ArrayList<>();
 
-        if (directory.isDirectory()) {
-            for (File file : directory.listFiles()) {
-                if (file.getName().toLowerCase().matches(LevelManager.LEVEL_FILE_PATTERN)) {
-                    levelFiles.add(file);
-                }
-            }
-        }
+		if (directory.isDirectory()) {
+			for (File file : directory.listFiles()) {
+				if (file.getName().toLowerCase().matches(LevelManager.LEVEL_FILE_PATTERN)) {
+					levelFiles.add(file);
+				}
+			}
+		}
 
-        return levelFiles;
-    }
+		return levelFiles;
+	}
 
-    /**
-     * Returns a {@link LevelInfo} of the {@link Level}.
-     *
-     * @param file - {@link File} containing the {@link Level} definition.
-     * @return the information about the {@link Level}; null if not found
-     */
-    private static LevelInfo getLevelInfo(File file) {
-        LevelInfo levelInfo = null;
-        Scanner fileScanner = null;
+	/**
+	 * Returns a {@link LevelInfo} of the {@link Level}.
+	 *
+	 * @param file
+	 *            - {@link File} containing the {@link Level} definition.
+	 * @return the information about the {@link Level}; null if not found
+	 */
+	private static LevelInfo getLevelInfo(File file) {
+		LevelInfo levelInfo = null;
+		Scanner fileScanner = null;
 
-        try {
-            fileScanner = new Scanner(file);
+		try {
+			fileScanner = new Scanner(file);
 
-            if (fileScanner.hasNextLine()) {
-            	String line = fileScanner.nextLine();
-            	System.out.println(line);
+			if (fileScanner.hasNextLine()) {
+				String line = fileScanner.nextLine();
+				System.out.println(line);
 
-            	Scanner lineScanner = new Scanner(line);
-            	lineScanner.useDelimiter(",");
-                int id = lineScanner.nextInt();
-                int width = lineScanner.nextInt();
-                int height = lineScanner.nextInt();
-                lineScanner.close();
+				Scanner lineScanner = new Scanner(line);
+				lineScanner.useDelimiter(",");
+				int id = lineScanner.nextInt();
+				int width = lineScanner.nextInt();
+				int height = lineScanner.nextInt();
+				lineScanner.close();
 
-                levelInfo = new LevelInfo(id, width, height, file);
-            }
-        } catch (Exception e) {
-            System.out.println("LevelManager: Error while loading file (" + file.getAbsolutePath() + ") with profiles.\n" + e.getMessage());
-        } finally {
-            if (fileScanner != null) {
-                fileScanner.close();
-            }
-        }
-        return levelInfo;
-    }
+				levelInfo = new LevelInfo(id, width, height, file);
+			}
+		} catch (Exception e) {
+			System.out.println("LevelManager: Error while loading file (" + file.getAbsolutePath()
+					+ ") with profiles.\n" + e.getMessage());
+		} finally {
+			if (fileScanner != null) {
+				fileScanner.close();
+			}
+		}
+		return levelInfo;
+	}
 
-    /**
-     * Returns a loaded {@link Level}.
-     *
-     * @param levelInfo - information about the {@link Level} to load.
-     * @return the loaded {@link Level}.
-     *
-     * @throws CollisionException when two cells are at the same position.
-	 * @throws ParsingException when trying to parse invalid data type.
-	 * @throws FileNotFoundException when level file is not found.
-     */
-    public static Level load(LevelInfo levelInfo) throws FileNotFoundException, CollisionException, ParsingException {
-        return LevelParser.parseLevel(levelInfo);
-    }
+	/**
+	 * Returns a loaded {@link Level}.
+	 *
+	 * @param levelInfo
+	 *            - information about the {@link Level} to load.
+	 * @return the loaded {@link Level}.
+	 *
+	 * @throws CollisionException
+	 *             when two cells are at the same position.
+	 * @throws ParsingException
+	 *             when trying to parse invalid data type.
+	 * @throws FileNotFoundException
+	 *             when level file is not found.
+	 */
+	public static Level load(LevelInfo levelInfo) throws FileNotFoundException, CollisionException, ParsingException {
+		return LevelParser.parseLevel(levelInfo);
+	}
 }
