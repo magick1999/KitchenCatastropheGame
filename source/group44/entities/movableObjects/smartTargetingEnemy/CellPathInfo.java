6625266342408e64588092dcc3ae6dff54d66536
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-07 22:45:30 +0000

    Create CellPathInfo.java

---
+++ source/group44/entities/movableObjects/smartTargetingEnemy/CellPathInfo.java
@@ -0,0 +1,98 @@
+package group44.entities.movableObjects.smartTargetingEnemy;
+
+import group44.entities.cells.Cell;
+import group44.entities.movableObjects.SmartTargetingEnemy;
+
+/**
+ * Holds the information needed about Cells to compute path for the
+ * {@link SmartTargetingEnemy}.
+ *
+ * @author Tomas Svejnoha
+ * @version 1.0
+ */
+public class CellPathInfo {
+	private int x; // position X
+	private int y; // position Y
+	private long cost; // how many steps to that cell; negative number mean not
+						// reacheable
+	private CellPathInfo parent;
+
+	/**
+	 * Creates a new instance of {@link CellPathInfo}.
+	 *
+	 * @param cell
+	 *            - cell
+	 */
+	public CellPathInfo(Cell cell) {
+		this.x = cell.getPositionX();
+		this.y = cell.getPositionY();
+
+		this.reset();
+	}
+
+	/**
+	 * Sets default cost.
+	 */
+	public void reset() {
+		this.cost = Long.MIN_VALUE;
+		this.parent = null;
+	}
+
+	/**
+	 * Returns position X of the cell described.
+	 *
+	 * @return x.
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * Returns position Y of the cell described.
+	 *
+	 * @return y.
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Sets the cost (number of steps needed to reach the cell). Use this method
+	 * JUST for the FIRST cell.
+	 *
+	 * @param cost
+	 *            - the cost.
+	 */
+	public void setCost(long cost) {
+		this.cost = cost;
+	}
+
+	/**
+	 * Returns the cost (number of steps needed to reach the cell).
+	 *
+	 * @return cost.
+	 */
+	public long getCost() {
+		return cost;
+	}
+
+	/**
+	 * Returns the parent {@link CellPathInfo}.
+	 *
+	 * @return the parent.
+	 */
+	public CellPathInfo getParent() {
+		return parent;
+	}
+
+	/**
+	 * Sets parent if the path is better.
+	 */
+	public void setParent(CellPathInfo newParent) {
+		// NO PARENT SO FAR or BETTER PATH FOUND
+		if ((this.parent == null) || (newParent.getCost() < this.parent.getCost())) {
+			this.parent = newParent;
+			this.setCost(newParent.getCost() + 1);
+		}
+	}
+}
