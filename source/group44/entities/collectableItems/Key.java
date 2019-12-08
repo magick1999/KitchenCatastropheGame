bbac1e1ac65bc4dea05fa18105fdb15a9ac99207
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-12-08 12:41:07 +0000

    Key: Formatting + toString()

--- source/group44/entities/collectableItems/Key.java
+++ source/group44/entities/collectableItems/Key.java
@@ -1,5 +1,6 @@
 package group44.entities.collectableItems;
 
+import group44.Constants;
 import group44.game.Level;
 
 /**
@@ -9,72 +10,88 @@ import group44.game.Level;
  * @version 1.0
  */
 public class Key extends CollectableItem {
-    private KeyType keyType;
+	private KeyType keyType;
 
-    /**
-     * Creates a new instance of {@link Key} of specific colour type.
-     *
-     * @param level     - The {@link Level} where the {@link Key} is located.
-     * @param type      - Colour type of the {@link Key}.
-     */
-    public Key(Level level, KeyType type) {
-        super(level, type.getTitle(), type.getImagePath());
-        this.keyType = type;
-    }
+	/**
+	 * Creates a new instance of {@link Key} of specific colour type.
+	 *
+	 * @param level
+	 *            - The {@link Level} where the {@link Key} is located.
+	 * @param type
+	 *            - Colour type of the {@link Key}.
+	 */
+	public Key(Level level, KeyType type) {
+		super(level, type.getTitle(), type.getImagePath());
+		this.keyType = type;
+	}
 
-    /**
-     * Returns the code of the key.
-     *
-     * @return key code represented as {@link Integer}.
-     */
-    public int getKeyCode() {
-        return this.keyType.getKeyCode();
-    }
+	/**
+	 * Returns the code of the key.
+	 *
+	 * @return key code represented as {@link Integer}.
+	 */
+	public int getKeyCode() {
+		return this.keyType.getKeyCode();
+	}
 
-    /**
-     * Represents all possible types of a {@link Key}. {@link KeyType} contains
-     * KeyCode and image path.
-     */
-    public enum KeyType {
-        RED(1, "Red key", "group44/resources/keys/red.png"), BLUE(2, "Blue key", "group44/resources/keys/blue.png"),
-        GREEN(3, "Green key", "group44/resources/keys/green.png"),
-        GOLD(4, "Gold key", "group44/resources/keys/yellow.png");
+	/**
+	 * Returns a string representation of a {@link Key}.
+	 *
+	 * @return a string representation of a {@link Key}.
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(Constants.TYPE_KEY);
+		sb.append(Constants.LEVEL_OBJECT_DELIMITER);
+		sb.append(this.getKeyCode());
+		return sb.toString();
+	}
 
-        private int code; // used to unlock the door (binary operation)
-        private String title;
-        private String imagePath;
+	/**
+	 * Represents all possible types of a {@link Key}. {@link KeyType} contains
+	 * KeyCode and image path.
+	 */
+	public enum KeyType {
+		RED(1, "Red key", "group44/resources/keys/red.png"), BLUE(2, "Blue key",
+				"group44/resources/keys/blue.png"), GREEN(3, "Green key",
+						"group44/resources/keys/green.png"), GOLD(4, "Gold key", "group44/resources/keys/yellow.png");
 
-        KeyType(int code, String title, String imagePath) {
-            this.code = code;
-            this.title = title;
-            this.imagePath = imagePath;
-        }
+		private int code; // used to unlock the door (binary operation)
+		private String title;
+		private String imagePath;
 
-        /**
-         * Returns the code of the key.
-         *
-         * @return key code represented as {@link Integer}.
-         */
-        public int getKeyCode() {
-            return this.code;
-        }
+		KeyType(int code, String title, String imagePath) {
+			this.code = code;
+			this.title = title;
+			this.imagePath = imagePath;
+		}
 
-        /**
-         * Returns the title (name) of the key.
-         *
-         * @return the title.
-         */
-        public String getTitle() {
-            return this.title;
-        }
+		/**
+		 * Returns the code of the key.
+		 *
+		 * @return key code represented as {@link Integer}.
+		 */
+		public int getKeyCode() {
+			return this.code;
+		}
 
-        /**
-         * Returns a path to the image of the key.
-         *
-         * @return a path represented as {@link String}.
-         */
-        public String getImagePath() {
-            return this.imagePath;
-        }
-    }
+		/**
+		 * Returns the title (name) of the key.
+		 *
+		 * @return the title.
+		 */
+		public String getTitle() {
+			return this.title;
+		}
+
+		/**
+		 * Returns a path to the image of the key.
+		 *
+		 * @return a path represented as {@link String}.
+		 */
+		public String getImagePath() {
+			return this.imagePath;
+		}
+	}
 }
